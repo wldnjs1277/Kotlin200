4b4257f46d6d2dff1705a405859b5e08bce1b0d0
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2022-02-07 17:00:57 +0900

    -Statement

---
+++ app/src/main/java/com/example/kotlin200/Statement.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin200
+
+//statement
+//statement(문장)은 독립적으로 실행할 수 있는 코틀린 코드 를 뜻한다.
+//문장은 독립적인 코드 조각을 새는 단위이기 때문에 표현식과는 조금 다르다.
+fun main(){
+    val num:Int//첫번째 문장
+    num=15//두번째 문장
+
+    println(num+7*3)//3번째 문장
+}
