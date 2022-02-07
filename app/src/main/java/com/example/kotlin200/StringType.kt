2247b12fba8d31f2688065175ca0993ccac4d4ba
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2022-02-07 16:16:30 +0900

    -StringType

---
+++ app/src/main/java/com/example/kotlin200/StringType.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin200
+
+//문자열 String
+
+fun main(){
+    var str:String="Hello"
+    println(str)
+    str = str+"\nKotlin!"
+    println(str)
+    println(str[0])
+    val num = 10*5+3
+    println(str+num)
+}
