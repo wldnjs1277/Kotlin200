1a5f169dec7febc0225715a1eb53d00dc7cb4c0a
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2022-02-07 17:49:42 +0900

    -LogicalOperator

--- app/src/main/java/com/example/kotlin200/LogicalOperator.kt
+++ app/src/main/java/com/example/kotlin200/LogicalOperator.kt
@@ -8,8 +8,7 @@ package com.example.kotlin200
 fun main(){
     val a = 15
     val b = 17
-    var bool:Boolean
-    bool = (a-b<a+b)&&(a==15)
+    var bool:Boolean = (a-b<a+b)&&(a==15)
     println(bool)
 
     bool = !((a+b)>(a*3)||(b-a)>0)
