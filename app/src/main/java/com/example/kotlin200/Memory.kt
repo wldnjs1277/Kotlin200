d805ad0c23e925be24097ab2fff4ba81636992c4
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2022-02-19 21:58:29 +0900

    -메모리의 스택영역

---
+++ app/src/main/java/com/example/kotlin200/Memory.kt
@@ -0,0 +1,26 @@
+package com.example.kotlin200
+
+//메모리의 스택영역
+
+//메모리에는 스택영역이 있다.
+//이곳에 변수가 저장되고 변수가 생성되는 순서대로 쌓여서 스택영역이라는 이름이 붙었다.
+
+fun main(){
+    val a = 1
+    val result = absolute(a)
+    println(result)
+    //a라는 변수가 스택영역에 가장먼저 쌓이고 result = absolute 함수에 a를 인자로 받는다
+    //absoulte 함수를 호출하여 number라는 매개변수가 생성 되어 스택영역에 쌓인다.
+    //absolute함수가 종료되면서 number가 스택영역에서 사라지고
+    //result가 스택영역에 생성된다.
+    //main함수가 종료되면 스택영역이 전부 사라진다.
+
+}
+
+fun absolute(number:Int):Int{
+    return if(number>=0){
+        number
+    }else{
+        -number
+    }
+}
