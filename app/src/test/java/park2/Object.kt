114809d380c2111e88575fc0e5c32ceb568ff638
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2022-02-20 19:40:17 +0900

    -object

---
+++ app/src/test/java/park2/Object.kt
@@ -0,0 +1,20 @@
+package park2
+
+// Object
+//객체는 소프트웨어 관점에서 연관있는 변수들을 묶어놓은 데이터 덩어리
+
+fun main(){
+    val myName="jiwon"
+    val myAge=27
+    println(myAge)
+    println(myName)
+
+    val myinformation=object {
+        //객체안의 변수들을 Property라고 한다.
+        val name:String="jiwon"
+        val age:Int=27
+    }
+
+    println(myinformation.age)
+    println(myinformation.name)
+}
