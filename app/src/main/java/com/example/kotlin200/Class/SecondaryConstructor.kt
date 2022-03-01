c5854155466dc32ab2c22f5cb625d5f9a0f8d951
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2022-03-01 22:09:23 +0900

    -보조생성자

--- app/src/main/java/com/example/kotlin200/Class/SecondaryConstructor.kt
+++ app/src/main/java/com/example/kotlin200/Class/SecondaryConstructor.kt
@@ -6,18 +6,19 @@ package com.example.kotlin200.Class
 //보조생성자는 기본생성자를 반드시 상속받아야 한다.
 
 fun main() {
-    val person=Person2("jiwon")
-    println(person.age)
+    val person=Person2("jiwon",27)
     val person1=Person2("jiwon",27,"lee")
+    println(person.name)
+    println(person1.name)
 
 
 }
-class Person2(var name:String,var age:Int){
+class Person2(var name:String){
     init {
         println("init실행")
     }
-    constructor(name:String):this(name, 27){
-        println("보조생성자")
+    constructor(name:String,age:Int):this("이름${name}나이${age}"){
+        println("1번째 보조생성자")
     }
     constructor(name: String,age: Int,firstname:String):this(firstname+name,age){
         println("2번째 보조생성자")
