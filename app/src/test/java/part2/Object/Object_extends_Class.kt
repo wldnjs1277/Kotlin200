14da59e0d5bf6ee624b0361d13551f268c7c4159
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2022-03-07 14:53:23 +0900

    -클래스를 상속하는 객체

---
+++ app/src/test/java/part2/Object/Object_extends_Class.kt
@@ -0,0 +1,22 @@
+package part2.Object
+
+//클래스를 상속하는 객체
+
+fun main(){         //object도 클래스 간에 상속하듯이 object: 뒤에 슈퍼클래스를 붙여주면된다.
+    val custom:Person=object:Person("jiwon",27)
+    {
+        override fun print() {
+            super.print()
+            println("object")
+        }
+    }
+    custom.print()
+}
+
+open class Person(val name:String,val age:Int){
+    open fun print(){
+        println("이름 = ${name}")
+        println("나이 = ${age}")
+    }
+}
+
