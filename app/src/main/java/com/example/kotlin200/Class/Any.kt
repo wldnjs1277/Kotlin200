e269e839070581cb1bf8a4a3470d228a0be4d9be
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2022-03-09 15:57:04 +0900

    -Any클래스

---
+++ app/src/main/java/com/example/kotlin200/Class/Any.kt
@@ -0,0 +1,28 @@
+package com.example.kotlin200.Class
+
+import kotlin.Any
+
+//Any
+
+//Any클래스는 모든 클래스의 슈퍼 클래스이다.
+//Any클래스에는 3가지 맴버 함수가 있다.
+//equals,hashCode,toString
+//equals는 ==과 같다.
+//hashCode는 객체 고유의 해시코드를 반환한다.
+//toString은 String타입으로 변환한다.
+
+class Building(val name:String="",val date:String="",val area:Int=0){
+    override fun toString(): String {
+      return "이름:${this.name}\n"+
+        "건축일자:${this.date}\n"+
+        "면적:${this.area}"
+    }
+}
+fun main(){
+    val building=Building("건물","2022",100)
+    val building2=Building()
+    println(building.toString())
+    println(building.hashCode())
+    println(building2.hashCode())
+    println(building.equals(building2))
+}
