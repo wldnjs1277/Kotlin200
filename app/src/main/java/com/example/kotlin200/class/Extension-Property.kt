b5e15f1b6582ed57bc02714e4b53c7c903ed2d3e
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2022-04-14 03:40:20 +0900

    -확장 프로퍼티

--- app/src/main/java/com/example/kotlin200/class/Extension-Property.kt
+++ app/src/main/java/com/example/kotlin200/class/Extension-Property.kt
@@ -1,3 +1,15 @@
 package com.example.kotlin200.`class`
 
-//과제 15일까지 다 끝내고 알아보기
+//확장 프로퍼티
+
+//문자열이 큰지 판단하는 확장 프로퍼티
+//확장 프로퍼티는 Field가 존재하지 않아서 field 식별자를 사용할 수 없다.
+val String.isLarge:Boolean
+get()=this.length>=10
+
+fun main(){
+    val a:String = "123456"
+    val b:String = "111111111111"
+    println(a.isLarge)
+    println(b.isLarge)
+}
