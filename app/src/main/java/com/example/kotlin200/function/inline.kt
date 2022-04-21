76c13d0a642bee11a05da9bf43fd92065558d81a
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2022-04-22 02:35:14 +0900

    -inline함수

--- app/src/main/java/com/example/kotlin200/function/inline.kt
+++ app/src/main/java/com/example/kotlin200/function/inline.kt
@@ -1,3 +1,14 @@
 package com.example.kotlin200.function
 
 //inline함수
+//inline함수는 재귀호출이 불가능하다.
+
+inline fun hello(){
+    println("hello")
+    println("kotlin")
+}
+
+fun main(){
+    hello()
+    hello()
+}
