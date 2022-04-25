05f48f13c00c1705ed1a83075cba82c828e82745
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2022-04-25 11:22:52 +0900

    -const

--- app/src/main/java/com/example/kotlin200/Const.kt
+++ app/src/main/java/com/example/kotlin200/Const.kt
@@ -1,3 +1,14 @@
 package com.example.kotlin200
 
 //const
+//val 변수앞에  const를 붙이면 변수에 접근하는 코드를 변수에 저장된 값으로 대체시킨다.
+
+const val hello="hello"+"world"
+
+object Foo{
+    const val bar="bar"
+}
+fun main(){
+    println(hello)
+    println(Foo.bar)
+}
