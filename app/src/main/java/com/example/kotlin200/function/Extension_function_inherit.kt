0d05d15434c5c62274b6527503ef8922a85394fa
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2022-05-17 15:46:45 +0900

    -확장함수

--- app/src/main/java/com/example/kotlin200/function/Extension_function_inherit.kt
+++ app/src/main/java/com/example/kotlin200/function/Extension_function_inherit.kt
@@ -2,8 +2,13 @@ package com.example.kotlin200.function
 
 //확장함수의 리시버 타입이 상속관계에 있을 때
 
+open class AAA
+class BBB:AAA()
 
+fun AAA.hello()= println("aaa")
+fun BBB.hello()=println("bbb")
 fun main(){
-
-
+val test:AAA=BBB()
+    test.hello()
 }
+//확장함수는 팜조변수가 실제로 가리키는 객체 타입을 따르지 않고 참조변수 타입을 그대로 따른다.
