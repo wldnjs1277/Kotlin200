858e7e2e69e9c1be6b1344b2e557c2212330698c
wldnj <wldnjs1277@naver.com>
2022-06-30 17:28:01 +0900

    -추상클래스

--- app/src/main/java/com/example/kotlin200/class/AbstractClass.kt
+++ app/src/main/java/com/example/kotlin200/class/AbstractClass.kt
@@ -1,21 +1,4 @@
 package com.example.kotlin200.`class`
-
-class Person(){
-    open fun getsalary()=0
-}
-//학생클래스
-class Student(private val money:Int):Person(){
-    override fun getsalary()= -money
-    }
-}
-//교수 클래스
-class Professor(private val classCount:Int):Person(){
-    override fun getsalary()= classCount*120
-}
-//직원 클래스
-class Employee(private val money:Int,private val year:Int):Person(){
-    override fun getsalary()=money*(1.0+year/10.0).toInt()
-}
 //////////////////////////////////////////////
 //클래스를 추상클래스로 만들려면 abstract키워드를 사용 추상클래스는 일부 맴버의 내용이 비어있는 불완전한 클래스
 //그러므로 객체를 생성할 수 없다.
@@ -25,8 +8,9 @@ class Employee(private val money:Int,private val year:Int):Person(){
 //공통되는 맴버를 전파하는 용도로 추상클래스를 사용
 //학생,교수,직원 클래스를 하나의 타입으로 묶어주는 person클래스
 
-abstract class PerSon(){
+abstract class PerSon{
     abstract fun getSalary():Int
+
 }
 //학생클래스
 class Student(private val money: Int):PerSon() {
@@ -38,9 +22,20 @@ class Professor(private val classCount:Int):PerSon(){
 }
 //직원클래스
 class Employee(private val money:Int,private val year:Int):PerSon(){
-    override fun getsalary()=money*(1.0+year/10.0).toInt()
+    override fun getSalary()=(money*(1.0+year/10.0)).toInt()
 }
-fun main(){
+//구성원으로 부터 학교 재정을 구함
+fun getFinance(vararg persons:PerSon):Int{
+    var i =0
+    var finance=0
+    while (i<persons.size){
+        finance-=persons[i].getSalary()
+        i+=1
+    }
+    return finance
+}
 
+fun main(){
+    println(123)
 }
 
