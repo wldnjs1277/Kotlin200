699a46c5c511f8a509051d7323e7c448e2347192
wldnj <wldnjs1277@naver.com>
2022-07-03 17:55:03 +0900

    -Interface

---
+++ app/src/main/java/com/example/kotlin200/Interface.kt
@@ -0,0 +1,21 @@
+package com.example.kotlin200
+
+//Interface
+//인터페이스는 클래스에 맴버 함수와 프로퍼티가 존재한다는 것을 보장하기 위한 장치
+
+
+interface Printable{
+    fun print()
+}
+class AA:Printable{
+    override fun print(){
+        println("AA")
+    }
+}
+fun print(anything:Printable){
+    anything.print()
+
+}
+fun main(){
+    print(AA())
+}
