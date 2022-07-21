11c6641d63323179dc6bf7f1fdbe97a0a232e752
wldnj <wldnjs1277@naver.com>
2022-07-21 14:09:47 +0900

    -중첩 클래스

--- app/src/main/java/com/example/kotlin200/class/NestedClass.kt
+++ app/src/main/java/com/example/kotlin200/class/NestedClass.kt
@@ -1,8 +1,13 @@
 package com.example.kotlin200.`class`
 
 //중첩 클래스
-
-
+//중첩 클래스는 형태만 내부에 존재할 뿐 실질적으로는 내용을 서로 공유할 수 없는 별개의 클래스
+class Outer{
+    class  Nested{
+        fun hello()= println("중첩된 클래스")
+    }
+}
 fun main(){
-
+    val  instance:Outer.Nested=Outer.Nested()
+    instance.hello()
 }
