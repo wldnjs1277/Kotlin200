a740c9d69165a5ac0c5a9c30787e2ff19a413eed
wldnj <wldnjs1277@naver.com>
2022-07-27 14:04:55 +0900

    -내부 클래스

--- app/src/main/java/com/example/kotlin200/class/InnerClass.kt
+++ app/src/main/java/com/example/kotlin200/class/InnerClass.kt
@@ -2,7 +2,21 @@ package com.example.kotlin200.`class`
 
 //내부클래스
 
+class Outer2(private val value:Int){
+    fun print(){
+        println(this.value)
+    }
 
+    inner class Inner(private val innervalue:Int){
+        fun print(){
+            println("Outer2 val= ${this@Outer2.print()}")
+            println("Inner val = ${this.innervalue}")
+            println("Outer2 val + Inner val = ${this.innervalue+this@Outer2.value}" )
+        }
+    }
+}
 fun main(){
-
+    val instance = Outer2(10)
+    val innerinstance:Outer2.Inner=instance.Inner(20)
+    innerinstance.print()
 }
