2b8a368d2bac210e6794703f67183136fab5c2c2
wldnj <wldnjs1277@naver.com>
2022-08-02 14:27:14 +0900

    -데이터 클래스

--- app/src/main/java/com/example/kotlin200/class/DataClass.kt
+++ app/src/main/java/com/example/kotlin200/class/DataClass.kt
@@ -2,7 +2,15 @@ package com.example.kotlin200.`class`
 
 //DataClass
 
+data class Employee1(val name:String,
+    val age:Int,val salary:Int)
 
 fun main(){
-
+    val first=Employee1("1번",11,300)
+    val second=Employee1("2번",22,600)
+    val third=first.copy()
+    println(first.toString())
+    println(second.toString())
+    println(first==second)
+    println(first==third)
 }
