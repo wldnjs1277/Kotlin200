3c9d6c4f4904ba33cf7df878f7a3b3cb5053d13f
wldnj <wldnjs1277@naver.com>
2022-08-25 15:57:59 +0900

    -

--- app/src/main/java/com/example/kotlin200/MainActivity.kt
+++ app/src/main/java/com/example/kotlin200/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.kotlin200
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+    //액티비티가 생성되었을때
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //레이아웃 설정
         setContentView(R.layout.activity_main)
-
-
+        Log.d(TAG, "onCreate: ")
     }
 }
