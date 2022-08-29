28265c761647b3b70b8cf8108e075431f23ec101
wldnj <wldnjs1277@naver.com>
2022-08-29 15:47:05 +0900

    -

--- app/src/main/java/com/example/kotlin200/class/DataClass.kt
+++ app/src/main/java/com/example/kotlin200/class/DataClass.kt
@@ -5,7 +5,6 @@ package com.example.kotlin200.`class`
 data class Employee1(val name:String,
     val age:Int,val salary:Int)
 
-
 fun main(){
     val first=Employee1("1번",11,300)
     val second=Employee1("2번",22,600)
