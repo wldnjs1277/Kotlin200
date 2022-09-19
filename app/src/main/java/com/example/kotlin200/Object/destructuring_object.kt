941c14644b320bf878a49a4f33097409d38ccbf5
wldnj <wldnjs1277@naver.com>
2022-09-19 16:00:35 +0900

    -

--- app/src/main/java/com/example/kotlin200/Object/destructuring_object.kt
+++ app/src/main/java/com/example/kotlin200/Object/destructuring_object.kt
@@ -4,6 +4,7 @@ package com.example.kotlin200.Object
 //----
 data class Employee(val name:String,val age:Int,val salary:Int)
 
+
 fun main(){
     //사용하지 않는 변수의 이름은 _를 지정하여 무시할 수 있다.
 val (name, _,salary)=Employee("jiwon",27,300)
