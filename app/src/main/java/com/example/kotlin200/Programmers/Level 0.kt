2d85ed8830941ebe4946ef18d5eacbad0d52b311
wldnj <wldnjs1277@naver.com>
2022-10-21 13:17:33 +0900

    -편지 문제

--- app/src/main/java/com/example/kotlin200/Programmers/Level 0.kt
+++ app/src/main/java/com/example/kotlin200/Programmers/Level 0.kt
@@ -1,11 +1,20 @@
 package com.example.kotlin200.Programmers
 fun main(){
-println(solution(64,6))
+println(solution("Happy birthday!"))
 }
+//편지문제
+fun solution(message: String): Int {
+    var answer: Int = 0
+    answer= message.length*2
+    return answer
+}
+
+
+
 //프로그래머스 양꼬치 문제
-fun solution(n: Int, k: Int): Int {
+/*fun solution(n: Int, k: Int): Int {
     var answer: Int = 0
     val a =kotlin.math.floor((n/10).toDouble())
     answer=(n*12000)+(k*2000)-(a.toInt()*2000)
     return answer
-}
+}*/
