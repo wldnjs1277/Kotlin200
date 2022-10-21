78b3e54db964233fb83857f10e4cf9ea97488297
wldnj <wldnjs1277@naver.com>
2022-10-21 13:53:09 +0900

    -중복된 숫자 갯수 문제

--- app/src/main/java/com/example/kotlin200/Programmers/Level 0.kt
+++ app/src/main/java/com/example/kotlin200/Programmers/Level 0.kt
@@ -1,15 +1,24 @@
 package com.example.kotlin200.Programmers
+
 fun main(){
-println(solution("Happy birthday!"))
+println(solution(1))
+
 }
+//중복된 숫자 갯수
+fun solution( n: Int): Int {
+    var answer: Int = 0
+    val array= arrayOf(1, 1, 2, 3, 4, 5)
+    answer= array.count {it==n}
+    return answer
+}
+
 //편지문제
+/*
 fun solution(message: String): Int {
     var answer: Int = 0
     answer= message.length*2
     return answer
-}
-
-
+}*/
 
 //프로그래머스 양꼬치 문제
 /*fun solution(n: Int, k: Int): Int {
